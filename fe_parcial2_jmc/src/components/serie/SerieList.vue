7e7e8da4d32285ec0257349b939e06544d50aec0
JoseMauricioC <josemauriciocamacho1@gmail.com>
2025-05-25 19:14:32 -0300

    CRUD en el front de la entidad series

--- fe_parcial2_jmc/src/components/serie/SerieList.vue
+++ fe_parcial2_jmc/src/components/serie/SerieList.vue
@@ -2,11 +2,13 @@
 import type { Serie } from '@/models/serie'
 import { onMounted, ref } from 'vue'
 import http from '@/plugins/axios'
-import { Button } from 'primevue'
+import { Button, Dialog } from 'primevue'
 
 const ENDPOINT = 'series'
 const series = ref<Serie[]>([])
 const emit = defineEmits(['edit'])
+const serieDelete = ref<Serie | null>(null)
+const mostrarConfirmDialog = ref<boolean>(false)
 
 async function obtenerLista() {
   series.value = await http.get(ENDPOINT).then((response) => response.data)
@@ -16,6 +18,16 @@ function emitirEdicion(serie: Serie) {
   emit('edit', serie)
 }
 
+function mostrarEliminarConfirm(serie: Serie) {
+  serieDelete.value = serie
+  mostrarConfirmDialog.value = true
+}
+
+async function eliminar() {
+  await http.delete(`${ENDPOINT}/${serieDelete.value?.id}`)
+  obtenerLista()
+  mostrarConfirmDialog.value = false
+}
 onMounted(() => {
   obtenerLista()
 })
@@ -46,11 +58,34 @@ defineExpose({ obtenerLista })
           <td>{{ serie.fechaEstreno }}</td>
           <td>
             <Button icon="pi pi-pencil" aria-label="Editar" text @click="emitirEdicion(serie)" />
-            <Button icon="pi pi-trash" aria-label="Eliminar" severity="danger" />
+            <Button
+              icon="pi pi-trash"
+              aria-label="Eliminar"
+              severity="danger"
+              @click="mostrarEliminarConfirm(serie)"
+            />
           </td>
         </tr>
       </tbody>
     </table>
+
+    <Dialog
+      v-model:visible="mostrarConfirmDialog"
+      header="Confirmar Eliminación"
+      :style="{ width: '25rem' }"
+    >
+      <p>¿Estás seguro de que deseas eliminar este registro?</p>
+      <br />
+      <div class="flex justify-end gap-2">
+        <Button
+          type="button"
+          label="Cancelar"
+          severity="secondary"
+          @click="mostrarConfirmDialog = false"
+        />
+        <Button type="button" label="Eliminar" @click="eliminar" />
+      </div>
+    </Dialog>
   </div>
 </template>
 
