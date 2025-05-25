61d87bc40c6c125164ab95f864659d7ff10ade4c
JoseMauricioC <josemauriciocamacho1@gmail.com>
2025-05-24 23:20:14 -0300

    listado en frntend con vue, falta crear, actualizar y eliminar

---
+++ fe_parcial2_jmc/src/components/serie/SerieList.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { Serie } from '@/models/serie'
+import { onMounted, ref } from 'vue'
+import http from '@/plugins/axios'
+import { Button } from 'primevue'
+
+const ENDPOINT = 'series'
+const series = ref<Serie[]>([])
+
+async function obtenerLista() {
+  series.value = await http.get(ENDPOINT).then((response) => response.data)
+}
+
+onMounted(() => {
+  obtenerLista()
+})
+defineExpose({ obtenerLista })
+</script>
+
+<template>
+  <div>
+    <table style="border-collapse: collapse" border="1">
+      <thead>
+        <tr>
+          <th>Nro.</th>
+          <th>Titulo</th>
+          <th>Sinopsis</th>
+          <th>Director</th>
+          <th>Temporadas</th>
+          <th>Fecha de Estreno</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(serie, index) in series" :key="serie.id">
+          <td>{{ index + 1 }}</td>
+          <td>{{ serie.titulo }}</td>
+          <td>{{ serie.sinopsis }}</td>
+          <td>{{ serie.director }}</td>
+          <td>{{ serie.temporadas }}</td>
+          <td>{{ serie.fechaEstreno }}</td>
+          <td>
+            <Button icon="pi pi-pencil" aria-label="Editar" text />
+            <Button icon="pi pi-trash" aria-label="Eliminar" severity="danger" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ fe_parcial2_jmc/src/views/SeriesView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SerieList from '@/components/serie/SerieList.vue'
+</script>
+
+<template>
+  <div class="contenedor">
+    <h1>Series</h1>
+    <SerieList />
+  </div>
+</template>
+
+<style scoped>
+.contenedor {
+  border: 2px solid white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
